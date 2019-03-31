4f702ec963fba884af4b6f64170552769e75e1a4
cvam0000 <cvam0000@gmail.com>
2019-03-31 23:30:32 +0530

    update

--- ohmyarch/settings.py
+++ ohmyarch/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'ohmyarch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
