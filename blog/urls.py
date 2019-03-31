6b295923c4c1117422591d3a16731cbd00f42d8f
cvam0000 <cvam0000@gmail.com>
2019-04-01 02:27:14 +0530

    django

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path ,include
-from .views import homePageView
+from .views import views
 
 urlpatterns = [
     
     path('',homePageView, name='home'),
+    path('posts/<slug:the_slug>/', views.post_detail, name='show_post'),
 ]
