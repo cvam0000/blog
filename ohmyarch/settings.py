eb7e8408487b1f7b8c9c5c6c80c475586b0230b8
cvam0000 <cvam0000@gmail.com>
2019-04-03 05:13:24 +0530

    origin master

--- ohmyarch/settings.py
+++ ohmyarch/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +26,7 @@ SECRET_KEY = 'v5t7jk*er*0%@9(tnlh#u61n7l%u=d%$juxx(k03h1gqcv0-&3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.176.244.101']
 
 
 # Application definition
@@ -128,5 +128,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
-django_heroku.settings(locals())
