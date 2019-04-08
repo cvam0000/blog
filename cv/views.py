a70ef7ecfef0bb2200d8bc17a49305e43d910a1c
cvam0000 <cvam0000@gmail.com>
2019-04-08 06:50:38 +0530

    profile

--- cv/views.py
+++ cv/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def cvPageView(request):
+    return render(request, 'cv/index.html')
--- ohmyarch/urls.py
+++ ohmyarch/urls.py
@@ -19,4 +19,5 @@ from django.urls import path ,include
 urlpatterns = [
     path('ardu/', admin.site.urls),
     path('',include('blog.urls')),
+    path('cv/',include('cv.urls')),
 ]
